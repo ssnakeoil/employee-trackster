5127b8a66d43fe133482886a6ff81acfebf41714
Stefi <114958335+ssnakeoil@users.noreply.github.com>
2023-04-02 11:00:30 -0400

    adding new employee prompts

--- db/index.js
+++ db/index.js
@@ -5,101 +5,110 @@ class DB {
   constructor(connection) {
     this.connection = connection;
   }
-
   // Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
   findAllEmployees() {
-    return this.connection.promise().query(
-      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
-    );
+    return this.connection
+      .promise()
+      .query(
+        "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+      );
   }
-
-  // Find all employees except the given employee id
-  filterOutId(employeeId) {
-    return this.connection.promise().query(
-      "SELECT id, first_name, last_name FROM employee WHERE id != ?",
-      employeeId
-    );
+  findAllEmployeesByDepartment(departmentId) {
+    return this.connection
+      .promise()
+      .query(
+        "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = ?;",
+        departmentId
+      );
   }
-
-  // Create new employee
-  newEmployee(employee) {
-    return this.connection.promise().query("INSERT INTO employee SET ?", employee);
+  findAllEmployeesByManager(managerId) {
+    return this.connection
+      .promise()
+      .query(
+        "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
+        managerId
+      );
   }
-
-  // Remove an employee with the given id
-  removeEmployee(employeeId) {
-    return this.connection.promise().query(
-      "DELETE FROM employee WHERE id = ?",
-      employeeId
-    );
-  }
-
-  // Update the given employee's role
-  updateEmployeeRole(employeeId, roleId) {
-    return this.connection.promise().query(
-      "UPDATE employee SET role_id = ? WHERE id = ?",
-      [roleId, employeeId]
-    );
-  }
-
-  // Update the given employee's manager
-  updateEmployeeManager(employeeId, managerId) {
-    return this.connection.promise().query(
-      "UPDATE employee SET manager_id = ? WHERE id = ?",
-      [managerId, employeeId]
-    );
-  }
-
   // Find all roles, join with departments to display the department name
   findAllRoles() {
-    return this.connection.promise().query(
-      "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
-    );
+    return this.connection
+      .promise()
+      .query(
+        "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+      );
   }
-
+  // Find all departments
+  findAllDepartments() {
+    return this.connection
+      .promise()
+      .query("SELECT department.id, department.name FROM department;");
+  }
+  // Find all employees except the given employee id
+  filterOutId(employeeId) {
+    return this.connection
+      .promise()
+      .query(
+        "SELECT id, first_name, last_name FROM employee WHERE id != ?",
+        employeeId
+      );
+  }
+  // Update the given employee's role
+  updateEmployeeRole(employeeId, roleId) {
+    return this.connection
+      .promise()
+      .query("UPDATE employee SET role_id = ? WHERE id = ?", [
+        roleId,
+        employeeId,
+      ]);
+  }
+  // Update the given employee's manager
+  updateEmployeeManager(employeeId, managerId) {
+    return this.connection
+      .promise()
+      .query("UPDATE employee SET manager_id = ? WHERE id = ?", [
+        managerId,
+        employeeId,
+      ]);
+  }
+  // Remove a role from the db
+  deleteRole(roleId) {
+    return this.connection
+      .promise()
+      .query("DELETE FROM role WHERE id = ?", roleId);
+  }
+  // Remove an employee with the given id
+  deleteEmployee(employeeId) {
+    return this.connection
+      .promise()
+      .query("DELETE FROM employee WHERE id = ?", employeeId);
+  }
+  deleteDepartment(departmentId) {
+    return this.connection
+      .promise()
+      .query("DELETE FROM department WHERE id = ?", departmentId);
+  }
+  // adds up dept budget by joining all employees and roles and finding sum of salaries
+  viewDepartmentBudgets() {
+    return this.connection
+      .promise()
+      .query(
+        "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;"
+      );
+  }
   // Create a new role
   addRole(role) {
     return this.connection.promise().query("INSERT INTO role SET ?", role);
   }
-
-  // Remove a role from the db
-  deleteRole(roleId) {
-    return this.connection.promise().query("DELETE FROM role WHERE id = ?", roleId);
-  }
-
-  // Find all departments
-  findAllDepartments() {
-    return this.connection.promise().query(
-      "SELECT department.id, department.name FROM department;"
-    );
-  }
-  // adds up dept budget by joining all employees and roles and finding sum of salaries
-  viewDepartmentBudgets() {
-    return this.connection.promise().query(
-      "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;"
-    );
-  }
   addDepartment(department) {
-    return this.connection.promise().query("INSERT INTO department SET ?", department);
+    return this.connection
+      .promise()
+      .query("INSERT INTO department SET ?", department);
   }
-  deleteDepartment(departmentId) {
-    return this.connection.promise().query(
-      "DELETE FROM department WHERE id = ?",
-      departmentId
-    );
-  }
-  findAllEmployeesByDepartment(departmentId) {
-    return this.connection.promise().query(
-      "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = ?;",
-      departmentId
-    );
-  }
-  findAllEmployeesByManager(managerId) {
-    return this.connection.promise().query(
-      "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
-      managerId
-    );
+  // Create new employee
+  newEmployee(employee) {
+    return this.connection
+      .promise()
+      .query("INSERT INTO employee SET ?", employee);
   }
 }
-
 module.exports = new DB(connection);
--- index.js
+++ index.js
@@ -13,72 +13,110 @@ function init() {
 }
 
 // Loads the main prompts
-function loadMainPrompts() {
-
-}
+function loadMainPrompts() {}
 
 // function that lets you view all employees
-function viewEmployees() {
-
-}
+function viewEmployees() {}
 
 // function that limits the view of employees by department
-function viewEmployeesByDepartment() {
-
-}
+function viewEmployeesByDepartment() {}
 
 // function that limits the view of employees by manager
-function viewEmployeesByManager() {
-
-}
+function viewEmployeesByManager() {}
 
 // function that lets you update employee role
-function updateEmployeeRole() {
-    
-}
+function updateEmployeeRole() {}
 
 // function that lets you update employee manager
-function updateEmployeeManager() {
-    
-}
+function updateEmployeeManager() {}
 
 // function that lets you delete an employee
-function deleteEmployee() {
-
-}
+function deleteEmployee() {}
 
 // function that lets you add employee
 function addEmployee() {
+  prompt([
+    {
+      name: "first_name",
+      message: "Please enter employee's first name:",
+    },
+    {
+      name: "last_name",
+      message: "Please enter employee's last name:",
+    },
+  ]).then((res) => {
+    let firstName = res.first_name;
+    let lastName = res.last_name;
 
+    db.findAllRoles().then(([rows]) => {
+      let roles = rows;
+      const roleChoices = roles.map(({ id, title }) => ({
+        name: title,
+        value: id,
+      }));
+
+      prompt({
+        type: "list",
+        name: "roleId",
+        message: "Employee's role:",
+        choices: roleChoices,
+      }).then((res) => {
+        let roleId = res.roleId;
+
+        db.findAllEmployees().then(([rows]) => {
+          let employees = rows;
+          const managerChoices = employees.map(
+            ({ id, first_name, last_name }) => ({
+              name: `${first_name} ${last_name}`,
+              value: id,
+            })
+          );
+
+          managerChoices.unshift({ name: "None", value: null });
+
+          prompt({
+            type: "list",
+            name: "managerId",
+            message: "Employee's manager:",
+            choices: managerChoices,
+          })
+            .then((res) => {
+              let employee = {
+                manager_id: res.managerId,
+                role_id: roleId,
+                first_name: firstName,
+                last_name: lastName,
+              };
+
+              db.createEmployee(employee);
+            })
+            .then(() =>
+              console.log(`Added ${firstName} ${lastName} to the database`)
+            )
+            .then(() => loadMainPrompts());
+        });
+      });
+    });
+  });
 }
 
 // function that lets you add a role
-function addRole() {
-
-}
+function addRole() {}
 
 // function that lets you delete a role
-function deleteRole() {
-
-}
+function deleteRole() {}
 
 // function that lets you add a department
-function addDepartment() {
-    
-}
+function addDepartment() {}
 
 // function that lets you delete a department
-function deleteDepartment() {
-
-}
+function deleteDepartment() {}
 
 // function that lets you view the total utilized budget of a department
-function viewUtilizedBudgets() {
-    
-}
+function viewUtilizedBudgets() {}
 
 // function that lets you exit the app
 function exit() {
-    console.log("Goodbye!");
-    process.exit();
+  console.log("Goodbye!");
+  process.exit();
 }
